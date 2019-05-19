f25ff5a6ca84dcad0e4dc98315b28da1bffb9439
AkrISrn <shairkarine@gmail.com>
2019-05-19 19:31:01 +0800

    fix style

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -103,7 +103,7 @@
             color #24292e
 
         h1, h2
-            border-bottom 1px solid #e4e4e4
+            border-bottom-color #e4e4e4
 
         img
             box-shadow 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)
@@ -127,6 +127,9 @@
                 &:before
                     content '»'
                     margin-right 8px
+
+        pre
+            background-color #2d2d2d
 
         .footnote-ref > a, a.footnote-backref
             color #0366d6
