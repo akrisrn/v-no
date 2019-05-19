0630c2ea0a9efb5451332612e9192295ee55b058
AkrISrn <shairkarine@gmail.com>
2019-05-19 18:45:43 +0800

    fix style

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -105,6 +105,10 @@
         h1, h2
             border-bottom 1px solid #e4e4e4
 
+        img
+            box-shadow 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)
+            background-color transparent
+
         mark
             border-radius 3px
             background-color rgba(255, 235, 59, 0.5)
