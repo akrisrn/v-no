8b4a5ba24092f250962a0a6b5e80e59724d2bfb7
AkrISrn <shairkarine@gmail.com>
2019-07-06 14:28:23 +0800

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,12 @@ export function error2markdown(error: AxiosError) {
 }
 
 export function getDate(path: string) {
-    const match = path.split('/').reverse()[0].match(/^\d{4}-\d{2}-\d{2}/);
-    return match ? new Date(match[0]) : null;
+    if (path) {
+        const match = path.split('/').reverse()[0].match(/^\d{4}-\d{2}-\d{2}/);
+        return match ? new Date(match[0]) : null;
+    } else {
+        return null;
+    }
 }
 
 export function getDateString(path: string) {
