307d60f7ff1c81af6570fcb4624e988bdfac44d9
AkrISrn <shairkarine@gmail.com>
2019-07-13 16:35:26 +0800

    fix

--- src/router.ts
+++ src/router.ts
@@ -4,11 +4,9 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '*',
-            name: 'index',
-            component: () => import('./views/Index.vue'),
-        },
-    ],
+    routes: [{
+        path: '*',
+        name: 'index',
+        component: () => import('./views/Index.vue'),
+    }],
 });
