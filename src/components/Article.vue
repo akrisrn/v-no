80bf597c1f0921810258b0c29afe17337dae5652
AkrISrn <shairkarine@gmail.com>
2019-07-16 17:32:20 +0800

    fix style

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -421,14 +421,12 @@
             border-bottom-color #e4e4e4
 
         img
+            margin-top 8px
             box-shadow 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)
             background-color transparent
 
             &.no-shadow
                 box-shadow none
-
-        p > img, dt > img
-            margin-top 8px
 
         mark
             border-radius 3px
