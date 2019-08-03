8169363c62df9b92c941f460bb459c9c57a99ec6
AkrISrn <shairkarine@gmail.com>
2019-08-03 18:44:26 +0800

    fix prerender.js

--- prerender.js
+++ prerender.js
@@ -27,8 +27,9 @@ const index = url.resolve(host, indexPath);
 
 function getLastUpdatedDate(filepath) {
     try {
-        const result = spawn.sync('git', ['log', '-1', '--format=%ct', filepath], {cwd: dir}).stdout.toString();
-        if (result) {
+        const result = spawn.sync('git', ['log', '-2', '--format=%ct', filepath], {cwd: dir}).stdout.toString();
+        // 略过小于两次提交的文件
+        if (result && result.split('\n').length === 3) {
             return new Date(parseInt(result) * 1000).toDateString()
         } else {
             return null
