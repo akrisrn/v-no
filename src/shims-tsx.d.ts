889e35bb616f0d4b84d288b189058757f8ff780f
AkrISrn <shairkarine@gmail.com>
2019-12-10 11:54:19 +0800

    small fix

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -3,13 +3,16 @@ import Vue, {VNode} from 'vue';
 declare global {
     namespace JSX {
         // tslint:disable no-empty-interface
+        // noinspection JSUnusedGlobalSymbols
         interface Element extends VNode {
         }
 
         // tslint:disable no-empty-interface
+        // noinspection JSUnusedGlobalSymbols
         interface ElementClass extends Vue {
         }
 
+        // noinspection JSUnusedGlobalSymbols
         interface IntrinsicElements {
             [elem: string]: any;
         }
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,5 @@
 declare module '*.vue' {
     import Vue from 'vue';
+    // noinspection JSUnusedGlobalSymbols
     export default Vue;
 }
