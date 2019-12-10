f84bee499b75bb46e7a637af351a031d6f2f090a
AkrISrn <shairkarine@gmail.com>
2019-12-10 11:35:07 +0800

    move markdown method to utils

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -12,11 +12,11 @@
         getQueryContent,
         getTime,
         getWrapRegExp,
+        renderMD,
         setFlag,
         splitTags,
     } from '@/utils';
     import axios from 'axios';
-    import MarkdownIt from 'markdown-it';
     import Prism from 'prismjs';
     import SmoothScroll from 'smooth-scroll';
     import {Component, Prop, PropSync, Vue} from 'vue-property-decorator';
@@ -34,42 +34,8 @@
             index: this.isIndex,
         }, 'markdown-body'];
 
-        // noinspection JSUnusedGlobalSymbols
-        public markdownIt = new MarkdownIt({
-            html: true,
-            breaks: true,
-            linkify: true,
-        }).use(require('markdown-it-footnote')).use(require('markdown-it-deflist'))
-            .use(require('markdown-it-task-lists')).use(require('markdown-it-container'), 'details', {
-                validate: (params: string) => {
-                    return params.trim().match(/^(open\s+)?(?:\.(.*?)\s+)?(.*)$/);
-                },
-                render: (tokens: { [index: number]: { nesting: number, info: string } }, idx: number) => {
-                    if (tokens[idx].nesting === 1) {
-                        const match = tokens[idx].info.trim().match(/^(open\s+)?(?:\.(.*?)\s+)?(.*)$/)!;
-                        let open = '';
-                        if (match[1]) {
-                            open = ' open';
-                        }
-                        let classAttr = '';
-                        if (match[2]) {
-                            classAttr = ` class="${match[2].split('.').join(' ')}"`;
-                        }
-                        const summary = this.markdownIt.render(match[3]).trim().replace(/^<p>(.*)<\/p>$/, '$1');
-                        return `<details${classAttr}${open}><summary>${summary}</summary>`;
-                    } else {
-                        return '</details>';
-                    }
-                },
-            });
-
         public get markdown() {
-            return this.renderMD(this.syncData);
-        }
-
-        // noinspection JSUnusedGlobalSymbols
-        public created() {
-            this.markdownIt.linkify.tlds([], false);
+            return renderMD(this.syncData, this.isCategory);
         }
 
         // noinspection JSUnusedGlobalSymbols
@@ -94,65 +60,6 @@
                 Prism.highlightAll();
                 this.updateTextCount();
             }, 0);
-        }
-
-        public renderMD(data: string, noToc = false) {
-            let article: HTMLElement;
-            const toc: string[] = [];
-            let firstHeader = '';
-            data = data.split('\n').map((line) => {
-                if (!noToc) {
-                    const tocMatch = line.match(getWrapRegExp('^(##+)', '$'));
-                    if (tocMatch) {
-                        if (!firstHeader) {
-                            firstHeader = tocMatch[1];
-                        }
-                        let prefix = '-';
-                        if (tocMatch[1] !== firstHeader) {
-                            prefix = tocMatch[1].replace(new RegExp(`${firstHeader}$`), '-').replace(/#/g, '  ');
-                        }
-                        toc.push(`${prefix} [${tocMatch[2]}](h${tocMatch[1].length})`);
-                    }
-                }
-                // 将被 $ 包围的部分作为 JavaScript 表达式执行
-                const jsExpMatches = line.match(getWrapRegExp('\\$', '\\$', 'g'));
-                if (jsExpMatches) {
-                    jsExpMatches.forEach((jsExpMatch) => {
-                        const m = jsExpMatch.match(getWrapRegExp('\\$', '\\$'))!;
-                        let result: string;
-                        try {
-                            if (!article) {
-                                article = document.createElement('article');
-                                article.innerHTML = this.markdownIt.render(data);
-                            }
-                            // tslint:disable-next-line:no-eval
-                            result = eval(`(function(article){${m[1]}})`)(article);
-                        } catch (e) {
-                            result = `${e.name}: ${e.message}`;
-                        }
-                        line = line.replace(m[0], result);
-                    });
-                    return line;
-                }
-                return line;
-            }).join('\n');
-            if (!noToc) {
-                let tocHtml = '<div id="toc">';
-                if (toc.length > 7 && !this.isCategory) {
-                    let mid = Math.ceil(toc.length / 2);
-                    while (toc[mid] && !toc[mid].startsWith('-')) {
-                        mid += 1;
-                    }
-                    tocHtml += this.markdownIt.render(toc.slice(0, mid).join('\n')) +
-                        this.markdownIt.render(toc.slice(mid, toc.length).join('\n'));
-                } else {
-                    tocHtml += this.markdownIt.render(toc.join('\n'));
-                }
-                tocHtml += '</div>';
-                tocHtml = tocHtml.replace(/<ul>/g, `<ul class="toc${this.isCategory ? ' tags' : ''}">`);
-                data = data.replace(/\[toc]/i, tocHtml);
-            }
-            return this.markdownIt.render(data);
         }
 
         public updateDD() {
@@ -408,7 +315,7 @@
                         }).join('\n');
                         // 规避递归节点重复问题。
                         try {
-                            a.parentElement!.outerHTML = this.renderMD(data, true);
+                            a.parentElement!.outerHTML = renderMD(data, this.isCategory, true);
                         } catch (e) {
                             return;
                         }
