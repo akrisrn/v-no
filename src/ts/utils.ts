1d38eb367d1b7300363c7367c32684b798c44300
AkrISrn <shairkarine@gmail.com>
2019-12-17 18:27:47 +0800

    add utils.trimList

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -2,6 +2,17 @@ export function getWrapRegExp(wrapLeft: string, wrapRight: string = wrapLeft, fl
     return new RegExp(`${wrapLeft}\\s*(.+?)\\s*${wrapRight}`, flags);
 }
 
+export function trimList(list: string[]) {
+    const result: string[] = [];
+    list.forEach((item) => {
+        const trim = item.trim();
+        if (trim) {
+            result.push(trim);
+        }
+    });
+    return result;
+}
+
 export function splitTags(tags: string) {
-    return tags.split(/\s*[,，]\s*/);
+    return trimList(tags.split(/\s*[,，]\s*/));
 }
