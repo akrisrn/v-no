4d87487594173703527c6596c33a2fdd43fd8346
AkrISrn <shairkarine@gmail.com>
2020-04-12 21:26:22 +0800

    add release.sh

--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 set -e
+. ./.env.sh
 yarn run build
 dist_path=$(pwd)/dist
-cd YOUR_RELEASE_ROOT_PATH
+cd "$RELEASE_PATH"
 git rm -rf css hash js
 cp -r "$dist_path/css" "$dist_path/hash" "$dist_path/js" .
 git add css hash js
