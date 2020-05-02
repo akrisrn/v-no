8bbb31a45f1d438c5e979eff1810a8009c3004bd
AkrISrn <shairkarine@gmail.com>
2020-05-02 19:41:26 +0800

    add IFlags

--- src/ts/enums.ts
+++ src/ts/enums.ts
@@ -1,7 +1,19 @@
 export enum EFlag {
+  // noinspection JSUnusedGlobalSymbols
   author = 'author',
   tags = 'tags',
   updated = 'updated',
   cover = 'cover',
   comment = 'comment',
+}
+
+export interface IFlags {
+  title?: string;
+  author?: string;
+  tags?: string;
+  updated?: string;
+  cover?: string;
+  comment?: string;
+
+  [index: string]: string | undefined;
 }
