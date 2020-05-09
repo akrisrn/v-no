3a711a65bc9874564c4ffe4dc48c9d973315b8bc
AkrISrn <shairkarine@gmail.com>
2020-05-09 16:15:20 +0800

    add assets/config.js

---
+++ public/assets/config.js
@@ -0,0 +1,12 @@
+vnoConfig = {
+  indexFile: 'index.md',
+  readmeFile: 'README.md',
+  categoryFile: 'categories.md',
+  archiveFile: 'archives.md',
+  searchFile: 'search.md',
+  commonFile: '',
+  untagged: 'untagged',
+  siteName: '',
+  author: 'root',
+  cdn: '',
+};
