6e143f2836a09e938afd27e4f86579f428e4852a
AkrISrn <shairkarine@gmail.com>
2020-11-03 23:37:30 +0800

    update splitFlags to get links

--- src/ts/file.ts
+++ src/ts/file.ts
@@ -1,7 +1,7 @@
 import { addBaseUrl, config, EFlag, getWrapRegExp, trimList } from '@/ts/utils';
 import axios, { AxiosError } from 'axios';
 
-function splitFlags(data: string) {
+function splitFlags(path: string, data: string): TMDFile {
   const flags: IFlags = {
     title: '',
     tags: [],
@@ -10,25 +10,36 @@ function splitFlags(data: string) {
   };
   const flagMarks = Object.values(EFlag).map(flag => `@${flag}:`);
   flagMarks.push('# ');
-  const regexp = getWrapRegExp(`^(${flagMarks.join('|')})`, '$', 'gm');
-  const dataCopy = data;
-  let match = regexp.exec(dataCopy);
-  while (match) {
-    if (match[1].startsWith('@')) {
-      const flag = match[1].substring(1, match[1].length - 1);
-      if ([EFlag.tags, EFlag.updated].map(flag => `@${flag}:`).includes(match[1])) {
-        flags[flag] = trimList(match[2].split(/\s*[,，、]\s*/));
+  const flagRegExp = getWrapRegExp(`^(${flagMarks.join('|')})`, '$');
+  const linkRegExp = /\[.*?]\((\/.*?\.md)\)/;
+  const links: string[] = [];
+  const lines: string[] = [];
+  data.split('\n').forEach(line => {
+    let match = line.match(flagRegExp);
+    if (match) {
+      if (match[1].startsWith('@')) {
+        const flag = match[1].substring(1, match[1].length - 1);
+        if ([EFlag.tags, EFlag.updated].map(flag => `@${flag}:`).includes(match[1])) {
+          flags[flag] = trimList(match[2].split(/\s*[,，、]\s*/));
+        } else {
+          flags[flag] = match[2];
+        }
       } else {
-        flags[flag] = match[2];
+        flags.title = match[2];
       }
     } else {
-      flags.title = match[2];
+      match = line.match(linkRegExp);
+      if (match) {
+        const linkPath = match[1];
+        if (linkPath !== path && !links.includes(linkPath)) {
+          links.push(linkPath);
+        }
+      }
+      lines.push(line);
     }
-    data = data.replace(match[0], '');
-    match = regexp.exec(dataCopy);
-  }
-  data = data.trim();
-  return { data, flags } as TMDFile;
+  });
+  data = lines.join('\n').trim();
+  return { path, data, flags, links };
 }
 
 const cachedFiles: TMDFileDict = {};
@@ -40,7 +51,7 @@ export function getFile(path: string, noCache = false) {
       resolve(cachedFiles[path]);
     } else {
       axios.get<string>(addBaseUrl(path)).then(response => {
-        cachedFiles[path] = splitFlags(response.data);
+        cachedFiles[path] = splitFlags(path, response.data);
         resolve(cachedFiles[path]);
       }).catch(error => {
         reject(error);
@@ -51,17 +62,12 @@ export function getFile(path: string, noCache = false) {
 
 async function walkFiles(files: TMDFile[]) {
   const paths: string[] = [];
-  const regexp = /\[.*?]\((\/.*?\.md)\)/gm;
   files.forEach(file => {
-    const data = file.data;
-    let match = regexp.exec(data);
-    while (match) {
-      const path = match[1];
-      if (!paths.includes(path) && cachedFiles[path] === undefined) {
+    file.links.forEach(path => {
+      if (cachedFiles[path] === undefined && !paths.includes(path)) {
         paths.push(path);
       }
-      match = regexp.exec(data);
-    }
+    });
   });
   if (paths.length > 0) {
     await walkFiles(await Promise.all(paths.map(path => getFile(path))));
