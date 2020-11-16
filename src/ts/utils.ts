c55d0578c6945e317fd464a2d62cc616c4554dd2
AkrISrn <shairkarine@gmail.com>
2020-11-16 14:04:37 +0800

    add utils.sortFiles

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,4 +1,4 @@
-import { config } from '@/ts/config';
+import { baseFiles, config } from '@/ts/config';
 import dayjs from 'dayjs';
 import SmoothScroll from 'smooth-scroll';
 
@@ -142,4 +142,62 @@ export function scroll(height: number, isSmooth = true) {
 // noinspection JSSuspiciousNameCombination
 export function getIcon(type: EIcon, width = 16, height = width) {
   return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="${width}" height="${height}"><path fill-rule="evenodd" d="${type}"></path></svg>`;
+}
+
+function comparePath(pathA: string, pathB: string) {
+  if (baseFiles.includes(pathA)) {
+    return baseFiles.includes(pathB) ? 0 : 1;
+  }
+  return baseFiles.includes(pathB) ? -1 : 0;
+}
+
+function compareDate(dateA: string, dateB: string) {
+  if (dateA) {
+    return dateB ? parseDate(dateB).getTime() - parseDate(dateA).getTime() : -1;
+  }
+  return dateB ? 1 : 0;
+}
+
+function compareTags(tagsA: string[], tagsB: string[]) {
+  if (tagsA.length > 0) {
+    if (tagsB.length > 0) {
+      if (tagsA.length === tagsB.length) {
+        for (let i = 0; i < tagsA.length; i++) {
+          const x = tagsA[i].localeCompare(tagsB[i]);
+          if (x !== 0) {
+            return x;
+          }
+        }
+        return 0;
+      }
+      return tagsB.length - tagsA.length;
+    }
+    return -1;
+  }
+  return tagsB.length > 0 ? 1 : 0;
+}
+
+function compareTitle(titleA: string, titleB: string) {
+  return titleA.localeCompare(titleB);
+}
+
+function comparePath2(pathA: string, pathB: string) {
+  return pathA.localeCompare(pathB);
+}
+
+export function sortFiles(fileA: TBacklinkFile, fileB: TBacklinkFile) {
+  let x = comparePath(fileA.path, fileB.path);
+  if (x === 0) {
+    x = compareDate(fileA.date, fileB.date);
+    if (x === 0) {
+      x = compareTags(fileA.tags, fileB.tags);
+      if (x === 0) {
+        x = compareTitle(fileA.title, fileB.title);
+        if (x === 0) {
+          x = comparePath2(fileA.path, fileB.path);
+        }
+      }
+    }
+  }
+  return x;
 }
