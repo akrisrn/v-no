7ea1102e77bfcdee1481d340ff3f1d5523a16279
AkrISrn <shairkarine@gmail.com>
2020-11-21 11:37:33 +0800

    remove PRISM_PLUGINS and PRISM_THEME from .env

--- babel.config.js
+++ babel.config.js
@@ -8,8 +8,8 @@ module.exports = {
       'prismjs',
       {
         languages: process.env.PRISM_LANGUAGES.split(',').map(language => language.trim()),
-        plugins: process.env.PRISM_PLUGINS.split(',').map(plugin => plugin.trim()),
-        theme: process.env.PRISM_THEME,
+        plugins: ['line-numbers', 'show-language', 'copy-to-clipboard'],
+        theme: 'tomorrow',
         css: true,
       },
     ],
