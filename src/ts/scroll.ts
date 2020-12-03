e7670562477d85263760bdef9aa446ff0e33be82
AkrISrn <shairkarine@gmail.com>
2020-12-03 12:25:31 +0800

    remove dep smooth-scroll

--- src/ts/scroll.ts
+++ src/ts/scroll.ts
@@ -1,13 +1,6 @@
-import SmoothScroll from 'smooth-scroll';
-
-const smoothScroll = new SmoothScroll(undefined, {
-  speed: 300,
-});
-
 export default function scroll(height: number, isSmooth = true) {
-  if (isSmooth) {
-    smoothScroll.animateScroll(height);
-  } else {
+  document.documentElement.style.scrollBehavior = !isSmooth ? 'auto' : 'smooth';
+  setTimeout(() => {
     scrollTo(0, height);
-  }
+  }, 0);
 }
