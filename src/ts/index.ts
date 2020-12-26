e851b4ab417bf7f8a224ed66cdcca528139e0792
AkrISrn <shairkarine@gmail.com>
2020-12-26 13:00:01 +0800

    expose store to window

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,7 @@ import * as config from '@/ts/config';
 import * as element from '@/ts/element';
 import * as enums from '@/ts/enums';
 import * as path from '@/ts/path';
+import * as store from '@/ts/store';
 import * as utils from '@/ts/utils';
 import { addInputBinds, destructors } from '@/ts/utils';
 import { exposeToWindow } from '@/ts/window';
@@ -16,5 +17,5 @@ export function bang() {
     },
     destructors,
   });
-  exposeToWindow({ config, element, enums, path, utils });
+  exposeToWindow({ config, element, enums, path, store, utils });
 }
