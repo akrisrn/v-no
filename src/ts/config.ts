793cfafc3de55a6127322ff67532f9dd52971966
AkrISrn <shairkarine@gmail.com>
2021-02-07 18:11:38 +0800

    fix config.merge

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -3,7 +3,7 @@ import { buildHash, shortenPath } from '@/ts/path';
 function merge(target: IConfig, source: IConfig) {
   for (const key of Object.keys(source)) {
     if (source[key] instanceof Object) {
-      Object.assign(source[key], merge(target[key], source[key]));
+      Object.assign(source[key], merge(target[key] || {}, source[key]));
     }
   }
   Object.assign(target || {}, source);
