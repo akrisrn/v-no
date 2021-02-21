27f2260480ecc4dc98277a22addf9434208f44e8
AkrISrn <shairkarine@gmail.com>
2021-02-21 19:21:38 +0800

    fix types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,16 @@
 type THeading = {
-  element: Element;
-  level: number;
-  isFolded: boolean;
-  children: (Element | THeading)[];
-  parent: THeading | null;
+  element: Element
+  level: number
+  isFolded: boolean
+  children: (Element | THeading)[]
+  parent: THeading | null
 }
 
 type TTagTree = {
-  [index: string]: TTagTree;
+  [index: string]: TTagTree
 }
 
-type TFileTs = typeof import('@/ts/async/file');
-type TMarkdownTs = typeof import('@/ts/async/markdown');
-type TUtilsTs = typeof import('@/ts/async/utils');
-type TPrismjsTs = typeof import('@/ts/async/prismjs');
+type TFileTs = typeof import('@/ts/async/file')
+type TMarkdownTs = typeof import('@/ts/async/markdown')
+type TUtilsTs = typeof import('@/ts/async/utils')
+type TPrismjsTs = typeof import('@/ts/async/prismjs')
