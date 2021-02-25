db550a42bcc5b4b00ee3b0eefb10998d4f5643ca
AkrISrn <shairkarine@gmail.com>
2021-02-26 00:31:16 +0800

    extend dayjs
    - relativeTime
    - utc

--- src/ts/async/date.ts
+++ src/ts/async/date.ts
@@ -2,9 +2,13 @@ import { config } from '@/ts/config';
 import dayjs from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(advancedFormat);
 dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 export function parseDate(date: string | number) {
   if (typeof date === 'string' && config.dateFormat) {
