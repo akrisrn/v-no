bd9eb6e7d1a91a51e5d2dc6623157346e00ba09e
AkrISrn <shairkarine@gmail.com>
2021-12-22 09:30:40 +0800

    feat(component): add App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionalComponent } from 'preact';
+
+const App: FunctionalComponent = () => {
+  return (
+    <div id="app" />
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import App from '@/components/App';
 import { render } from 'preact';
 
-render(<div />, document.getElementById('app')!);
+render(<App />, document.getElementById('app')!);
