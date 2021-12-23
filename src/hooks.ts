54f2252c15a5ab67f045327dc794ad358cee6f3c
AkrISrn <shairkarine@gmail.com>
2021-12-23 18:19:32 +0800

    feat(hook): add useRegistry

---
+++ src/hooks.ts
@@ -0,0 +1,35 @@
+import { StateUpdater, useEffect } from 'preact/hooks';
+
+const registry: Record<string, unknown> = {};
+
+function register<T>(name: string, setter: StateUpdater<T>) {
+  Object.defineProperty(registry, name, {
+    configurable: true,
+    enumerable: true,
+    async get() {
+      return await new Promise<T>(resolve => {
+        setter(prev => {
+          resolve(prev);
+          return prev;
+        });
+      });
+    },
+    set(value: T) {
+      setter(value);
+    },
+  });
+}
+
+function deregister(name: string) {
+  delete registry[name];
+}
+
+export function useRegistry<T>(name: string, setter: StateUpdater<T>) {
+  useEffect(() => {
+    register(name, setter);
+    return () => {
+      deregister(name);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
